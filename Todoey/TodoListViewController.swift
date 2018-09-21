a560e1ed691dc8418f879153f686bd267d6e4171
Felix Wahlberg <wahlbergf@msn.com>
2018-09-21 22:58:46 +0100

    Add item functionality

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Felix Wahlberg on 21/09/2018.
+//  Copyright © 2018 Felix Wahlberg. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController{
+    var itemArray = ["Find Felix", "Buy Egss", "Buy Nixie"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    //MARK - Add New Items
+    @IBAction func addNewItem(_ sender: Any) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+
+}
+
---
+++ Todoey/View/CustomToDoCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomToDoCell.swift
+//  Todoey
+//
+//  Created by Felix Wahlberg on 21/09/2018.
+//  Copyright © 2018 Felix Wahlberg. All rights reserved.
+//
+
+import UIKit
+
+class CustomToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
